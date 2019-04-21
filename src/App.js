ab3ba1b2bc16fe3c11f9e14dc460b0a586757d0e
Waheed Afolabi <waheed.freelancer16@gmail.com>
2019-04-21 22:13:41 +0100

    feature(image-link-component): add image link component add an input for user to enter image url create a component for it add rank component add a placeholder for text [Finishes #165486336]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +11,9 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
-        {/*
+        <Rank />
         <ImageLinkForm />
+        {/*
         <ImageRecognition />
         */}        
       </div>
